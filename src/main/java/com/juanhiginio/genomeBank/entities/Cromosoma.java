e6eeb5cffab59177ce14ba5428e7deb0be1b7389
sgrajalesc <117198970+sgrajalesc@users.noreply.github.com>
2025-10-19 17:05:10 -0500

    build: All code is developed within the entity located in src/main/java/com/juanhiginio/genomeBank/entities/Cromosoma.java

--- src/main/java/com/juanhiginio/genomeBank/entities/Cromosoma.java
+++ src/main/java/com/juanhiginio/genomeBank/entities/Cromosoma.java
@@ -1,10 +1,12 @@
 package com.juanhiginio.genomeBank.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "Cromosoma")
@@ -12,4 +14,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class Cromosoma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "longitudParesDeBases")
+    private int longitudParesDeBases;
+
+    @Column(name = "secuenciaADN", nullable = false)
+    private String secuenciaADN;
+    /*Tengo dudas aqui, dentro de la tabla en la base de datos dice que es tipo de dato TEXT, y en el texto dice que este campo debe de ser unicamente en alfabeto
+    Entonces porfa para revisarlo si se define como String o se especifica tipo "[a-zA-Z]*"
+     */
+
+    @ManyToOne
+    @JoinColumn(name = "genoma_id", nullable = false)
+    private Genoma genoma;
+
+    @OneToMany(mappedBy = "cromosoma", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Gen> genes = new HashSet<>();
+
 }
