e4e5fdd3d56b73996ac0830841d82b9159dfab73
juanhiginio <juanhiginio06@gmail.com>
2025-10-19 03:00:18 -0500

    feat: Create entity Rol

---
+++ src/main/java/com/juanhiginio/genomeBank/entities/Rol.java
@@ -0,0 +1,11 @@
+package com.juanhiginio.genomeBank.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "rol")
+@Data
+public class Rol {
+}
