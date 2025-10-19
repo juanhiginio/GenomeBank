69fcd3cf23bd5f4e0d27b9d4beae882fea2ec59a
juanhiginio <juanhiginio06@gmail.com>
2025-10-19 01:42:13 -0500

    feat: Create project structure

---
+++ src/main/java/com/juanhiginio/genomeBank/GenomeBankApplication.java
@@ -0,0 +1,13 @@
+package com.juanhiginio.genomeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenomeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenomeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanhiginio/genomeBank/GenomeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanhiginio.genomeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenomeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
