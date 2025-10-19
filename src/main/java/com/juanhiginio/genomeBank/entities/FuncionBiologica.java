9f012fd07d5ea9e62bfc556fd2291bd800cc9e6a
juanhiginio <juanhiginio06@gmail.com>
2025-10-19 01:53:25 -0500

    feat: Create GenomeBank entities structure

---
+++ src/main/java/com/juanhiginio/genomeBank/entities/FuncionBiologica.java
@@ -0,0 +1,15 @@
+package com.juanhiginio.genomeBank.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "FuncionBiologica")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FuncionBiologica {
+}
---
+++ src/main/java/com/juanhiginio/genomeBank/entities/Gen.java
@@ -0,0 +1,15 @@
+package com.juanhiginio.genomeBank.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Gen")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Gen {
+}
